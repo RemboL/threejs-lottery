f138852f6bb7c51084036c76b21e4095642467ae
Milosz Rembisz <r3mbol@gmail.com>
2023-11-02 06:44:44 +0100

    Input names

---
+++ src/InputNames.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+import './InputNames.css';
+
+export interface InputNamesProps {
+    inputNames: (names: string[]) => void;
+}
+
+export default function InputNames(props: InputNamesProps) {
+    const [namesAsString, setNamesAsString] = useState('');
+
+    const names = namesAsString
+        .split('\n')
+        .map(line => line.trim())
+        .filter(line => line.length > 0);
+
+    return <>
+        <div className='modal show' style={{ display: 'block', position: 'initial' }}>
+            <Modal.Dialog contentClassName='input-names-content'>
+                <Modal.Header>
+                    <Modal.Title>Enter names</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <textarea
+                            className='names-textarea'
+                            value={namesAsString}
+                            onChange={(e) => {setNamesAsString(e.target.value)}}
+                        />
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" disabled={names.length === 0} onClick={() => props.inputNames(names)}>Submit</Button>
+                </Modal.Footer>
+        </Modal.Dialog>
+      </div>
+    </>;
+}
---
+++ src/Phase.ts
@@ -0,0 +1,8 @@
+enum Phase {
+  INPUTTING_DATA,
+  INITIATING_MACHINE,
+  RUNNING_LOTTERY,
+  DISPLAYING_WINNER
+}
+
+export default Phase;
